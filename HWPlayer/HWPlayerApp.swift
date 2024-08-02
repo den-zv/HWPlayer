52d2348042e496fb6d40c5a43f2e39dd6b0b94f2
Denys Zabrodko <den.in.fl@gmail.com>
2024-08-02 22:29:51 +0300

    chore: initial commit

---
+++ HWPlayer/HWPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWPlayerApp.swift
+//  HWPlayer
+//
+//  Created by Denis on 02.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct HWPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
